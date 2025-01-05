352272da45a4bd5fc414824f1b652cb3ca4fea7c
famemonkey <redbirdhasflown@gmail.com>
2025-01-05 19:30:20 +0100

    changed the title and the subtitle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  template: `
+    <h1>Your recipe book!</h1>
+    <h2>Just search for what you are hungry for!</h2> 
+  `,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
