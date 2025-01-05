ecc90643d7ff11c4bc9bcc9e25b40714fa946280
famemonkey <redbirdhasflown@gmail.com>
2025-01-05 19:45:23 +0100

    app-root: include the search bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { SearchbarComponent } from './searchbar/searchbar.component';
 
 @Component({
   selector: 'app-root',
   template: `
     <h1>Your recipe book!</h1>
-    <h2>Just search for what you are hungry for!</h2> 
+    <h3>Just search for what you are hungry for</h3> 
+    <app-searchbar>
   `,
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [SearchbarComponent],
 })
 export class AppComponent {
   title = 'recipes';
